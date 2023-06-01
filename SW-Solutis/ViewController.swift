de05ff09e2b4cbae58f727676bd418ce24d6b88d
Alexandre <alexandrefreitas.dev@gmail.com>
2023-06-01 16:28:57 +0200

    Initial Commit

---
+++ SW-Solutis/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SW-Solutis
+//
+//  Created by Alexandre Freitas on 01/06/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
